ea03f0ff99a55e0bc8f59f8237ed8a72aaaa5f55
William <william.sizemore@mavs.uta.edu>
2017-11-11 21:00:42 -0600

    LoginAndReg initial UI created

--- app/src/main/java/project/cse3310/Main.java
+++ app/src/main/java/project/cse3310/Main.java
@@ -1,13 +1,31 @@
 package project.cse3310;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewAnimator;
+import android.widget.ViewFlipper;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_main);
+    }
+
+    /* viewFlipper to registration form in activity_login */
+    public void register(View view) {
+        ViewAnimator vf = findViewById(R.id.viewFlipper);
+        vf.getInAnimation();
+        vf.showNext();
+    }
+    public void validateLogin(View view) {
+        LoginAndReg login = new LoginAndReg();
+
     }
 }
